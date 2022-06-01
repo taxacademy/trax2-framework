2149f5559de689645b551357a3fbadf7884db1c1
Daniel Band <daniel.band@tax-academy.de>
2022-06-01 17:23:27 +0200

    sdfsdf

--- xapi-store/src/Stores/Statements/XapiStatementContentValidation.php
+++ xapi-store/src/Stores/Statements/XapiStatementContentValidation.php
@@ -78,8 +78,11 @@ trait XapiStatementContentValidation
     {
         Log::channel('benchmark')->info('validateStatementMultiparts');
         // Content-Type.
+        if (empty($parts)) {
+            throw new XapiBadRequestException('Empty multi parts');
+        }
         Log::channel('benchmark')->info('Length: ' . $parts[0]->length);
-        $statements = $parts[0];
+        $statements = array_shift($parts);
         Log::channel('benchmark')->info('Length: ' . json_encode($statements));
         if (!isset($statements->contentType) || $statements->contentType != 'application/json') {
             throw new XapiBadRequestException('Invalid Content-Type in multipart request.');
